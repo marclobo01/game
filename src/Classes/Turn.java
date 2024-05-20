29ec45786c95cf0313489cdb9bd17cf9ada3245f
João Marcelo <marcelolobo.matos@gmail.com>
2024-05-20 17:58:20 -0300

    turns melhor organizado

--- src/Classes/Turn.java
+++ src/Classes/Turn.java
@@ -1,12 +1,15 @@
 package Classes;
 
 import java.util.ArrayList; // import the ArrayList class
+import java.util.Scanner;
+import java.util.Random;
 
 public class Turn {
 	
 	ArrayList<Creatures> jogo = new ArrayList<Creatures>();
 	
-	
+	Scanner scan = new Scanner(System.in);
+	Random rand = new Random();
 	
 	public void start(Perso p, Perso q, Perso r, Mob a, Mob b) {
 		if(p.vel >= q.vel) {
@@ -55,8 +58,48 @@ public class Turn {
 			Creatures aux = jogo.get(i);
 			
 			if(aux instanceof Perso) {
-				
+				System.out.println("O personagem é: " + ((Perso) aux).name + " escolha o que fazer.");
+				int choice = scan.nextInt();
+				switch (choice) {
+					case 1:  // atacar um inimigo
+						choice = scan.nextInt();
+						if(choice == 1) {
+							((Perso) aux).atacar(a);
+						}else if(choice == 2) {
+							((Perso) aux).atacar(b);
+						}
+						break;
+					case 2:
+						if(aux instanceof Healer) {
+							choice = scan.nextInt();
+							if(choice == 1) {
+								((Healer) aux).skill(p);
+							}else if(choice == 2) {
+								((Healer) aux).skill(q);
+							}else if(choice == 3) {
+								((Healer) aux).skill(r);
+							}
+						}else {
+							choice = scan.nextInt();
+							if(choice == 1) {
+								((Archer) aux).skill(a);
+							}else if(choice == 2) {
+								((Archer) aux).skill(b);
+							}
+						}
+						break;
+				}
 			}else {
+				int x = p.agro + q.agro + r.agro;
+				int value = rand.nextInt(x); // um numero entre 0 e x-1;
+				
+				if(value >= 0 && value < p.agro) {
+					((Mob) aux).atack(p);
+				}else if(value >= p.agro && value < (p.agro + q.agro)) {
+					((Mob) aux).atack(q);
+				}else {
+					((Mob) aux).atack(r);
+				}
 				
 			}
 		}
--- src/main/main.java
+++ src/main/main.java
@@ -4,11 +4,21 @@ import Classes.Archer;
 import Classes.Healer;
 import Classes.Mob; // vida atq def
 import Classes.Perso; // vida atq vel def
+import Classes.Turn;
 
 public class main {
 
 	public static void main(String[] args) {
 		
+		Archer arch = new Archer("Legolas", 500, 120, 160, 100, 50);
+		Archer arch2 = new Archer("Lucas", 600,100,150,70,80);
+		Healer heal = new Healer("Sara",900,80,120,80,60);
+		
+		Mob goblin = new Mob("Goblin", 500, 120, 100);
+		Mob alf = new Mob("Ticotico", 800, 100, 50);
+		
+		Turn novo = new Turn();
+		novo.start(heal, arch, arch2, alf, goblin);
 		
 	}
 
